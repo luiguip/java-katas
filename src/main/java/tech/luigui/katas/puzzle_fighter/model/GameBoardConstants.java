28b48623a60c3390060a0432297a55c241c74351
Luigui de A <luisguilhermepdea@gmail.com>
2021-01-02 11:38:39 -0300

    GameBoardConstants to static

--- src/main/java/tech/luigui/katas/puzzle_fighter/model/GameBoardConstants.java
+++ src/main/java/tech/luigui/katas/puzzle_fighter/model/GameBoardConstants.java
@@ -2,13 +2,23 @@ package tech.luigui.katas.puzzle_fighter.model;
 
 public class GameBoardConstants {
 
-	private  final int numberOfRows = 12;
-	private  final int numberOfColumns = 6;
+	private static final int numberOfRows = 12;
+	private static final int numberOfColumns = 6;
 
-	public int getNumberOfRows() {
+	private static final int blockFallColumn = 3;
+	private static final int lastRow = numberOfRows-1;
+
+	public static int getNumberOfRows() {
 		return numberOfRows;
 	}
-	public int getNumberOfColumns() {
+	public static int getNumberOfColumns() {
 		return numberOfColumns;
+	}
+
+	public static int getBlockFallColumn() {
+		return blockFallColumn;
+	}
+	public static int getLastRow() {
+		return lastRow;
 	}
 }
