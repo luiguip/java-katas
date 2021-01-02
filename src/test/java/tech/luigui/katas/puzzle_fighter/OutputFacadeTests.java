40ca140eb99195aadec3266d8d161d6f1c2e81e3
Luigui de A <luisguilhermepdea@gmail.com>
2021-01-02 11:48:22 -0300

    Test added to OutputFacadeTests.java

--- src/test/java/tech/luigui/katas/puzzle_fighter/OutputFacadeTests.java
+++ src/test/java/tech/luigui/katas/puzzle_fighter/OutputFacadeTests.java
@@ -4,20 +4,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import tech.luigui.katas.puzzle_fighter.model.AlivePieceCoordinate;
 import tech.luigui.katas.puzzle_fighter.model.GameBoard;
+import tech.luigui.katas.puzzle_fighter.model.Input;
 
 class OutputFacadeTests {
 
 	private final String EMPTY_BOARD = 
 			"      \n      \n      \n      \n      \n      \n      \n      \n      \n      \n      \n      ";
-	private final GameBoard emptyGameBoard = new GameBoard();
-	private final OutputFacade outputFacadeEmptyBoard = new OutputFacade(emptyGameBoard);
-	
+	private final String INITIAL_STATE = 
+			"   R  \n   B  \n      \n      \n      \n      \n      \n      \n      \n      \n      \n      ";
+	private final String ONE_FALL_STATE =
+			"      \n   R  \n   B  \n      \n      \n      \n      \n      \n      \n      \n      \n      ";
+	private final String NO_MOVE =
+			"      \n      \n      \n      \n      \n      \n      \n      \n      \n      \n   R  \n   B  ";
+	private final GameBoardManager gameBoardManager = new GameBoardManager();
+
 	@Test
-	void testEmptyBoard() {
-		assertEquals(EMPTY_BOARD, outputFacadeEmptyBoard.getGameOutput());
+	void initialStateBoard() {
+		Input input = new Input("RB", "");
+		GameBoard gameBoard = gameBoardManager.createInitialBoard(input.getPieces());
+		OutputFacade outputFacade = new OutputFacade(gameBoard);
+		assertEquals(INITIAL_STATE, outputFacade.getGameOutput());
+	}
+
+	@Test
+	void fallOneBlockStateBoard() {
+		Input input = new Input("RB", "");
+		GameBoard initialGameBoard = gameBoardManager.createInitialBoard(input.getPieces());
+		AlivePieceCoordinate alivePieceCoordinate = initialGameBoard.getAlivePieceCoordinate().fall();
+		GameBoard gameBoard = gameBoardManager.update(initialGameBoard,alivePieceCoordinate);
+		OutputFacade outputFacade = new OutputFacade(gameBoard);
+		assertEquals(ONE_FALL_STATE, outputFacade.getGameOutput());
 	}
 	
-	
+//	@Test
+//	void noMoveBoard() {
+//		Input input = new Input("RB", "");
+//		assertEquals(NO_MOVE, outputFacadeEmptyBoard.getGameOutput());
+//	}
+//	
 	
 }
