4bbdd2fb1348b5343f119845c1b75297001d4c5e
Luigui de A <luisguilhermepdea@gmail.com>
2021-01-02 11:41:00 -0300

    No Movement added

--- src/main/java/tech/luigui/katas/puzzle_fighter/model/MovementEnum.java
+++ src/main/java/tech/luigui/katas/puzzle_fighter/model/MovementEnum.java
@@ -5,7 +5,8 @@ public enum MovementEnum {
 	LEFT("L"),
 	RIGHT("R"),
 	ROTATE_COUNTERCLOCKWISE("A"),
-	ROTATE_CLOCKWISE("B");
+	ROTATE_CLOCKWISE("B"),
+	NO_MOVE("");
 
 	private final String rawMovement;
 	
@@ -19,16 +20,18 @@ public enum MovementEnum {
 	
 	public static MovementEnum getByRawMovement(String rawMovement) {
 		switch(rawMovement) {
-		case "L":
-			return MovementEnum.LEFT;
-		case "R":
-			return MovementEnum.RIGHT;
-		case "A":
-			return MovementEnum.ROTATE_COUNTERCLOCKWISE;
-		case "B":
-			return MovementEnum.ROTATE_CLOCKWISE;
-		default:
-			throw new IllegalArgumentException();
+			case "L":
+				return MovementEnum.LEFT;
+			case "R":
+				return MovementEnum.RIGHT;
+			case "A":
+				return MovementEnum.ROTATE_COUNTERCLOCKWISE;
+			case "B":
+				return MovementEnum.ROTATE_CLOCKWISE;
+			case "":
+				return MovementEnum.NO_MOVE;
+			default:
+				throw new IllegalArgumentException();
 		}
 	}
 }
